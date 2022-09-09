a0d3dea6fe0f9522a6a377b4b2950f2f20b1138a
Stacey <stacey.graham1106@gmail.com>
2022-09-08 22:10:54 -0400

    updated subgraph url

--- apollo-client.js
+++ apollo-client.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://api.thegraph.com/subgraphs/name/stashag/rsvpsubgraph",
+  uri: "https://api.thegraph.com/subgraphs/name/stashag/web3rsvp-subgraph",
   cache: new InMemoryCache(),
 });
 
